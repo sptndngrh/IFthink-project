427fbf7544482ccdc6300180122ff94cc16c1481
sptndngrh <septiandin92@gmail.com>
2023-04-06 13:14:51 +0700

    Menambahkan versi 0.0.1

---
+++ assets/python/search.py
@@ -0,0 +1,11 @@
+import webbrowser
+website = input("Search Website: ")
+
+if website == "google":
+    google = input("Search: ")
+    webbrowser.open("https://www.google.com/search?q= " + google)
+elif website == "youtube":
+    print("h1")
+elif website == "musescore":
+    print("h1")
+
